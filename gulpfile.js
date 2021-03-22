71531e8c51e5a47741ebe035850248ded0aa0372
Martin Dhenu <martin.dhenu@chefkoch.de>
2021-03-22 13:35:04 +0100

    FR-SITE: manage js assets with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
-function defaultTask(cb) {
-    console.log('Gulp successfully installed')
-    cb();
-}
+const { src, dest } = require('gulp');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 
-exports.default = defaultTask
+exports.default = function() {
+    return src('site/templates/dist/js/*.js')
+        .pipe(babel())
+        .pipe(dest('site/templates/public/js/'))
+        .pipe(uglify())
+        .pipe(rename({ extname: '.min.js' }))
+        .pipe(dest('site/templates/public/js/'));
+}
