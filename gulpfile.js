1a79600d2de8e7ffe0a74d707a31122f3653b51c
Martin Dhenu <martin.dhenu@chefkoch.de>
2021-03-22 14:18:29 +0100

    FR-SITE: define tasks for css compile + execute it with js in parallel

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,31 @@
-const { src, dest } = require('gulp');
+const { src, dest, parallel, task } = require('gulp');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer')
+cleanCSS = require('gulp-clean-css')
+purgecss = require('gulp-purgecss')
 
-exports.default = function() {
-    return src('site/templates/dist/js/*.js')
+const paths = {
+    src: 'site/templates/dist/',
+    dest: 'site/templates/public/'
+}
+
+task('build-js', function(cb) {
+    return src(paths.src + 'js/*.js')
         .pipe(babel())
-        .pipe(dest('site/templates/public/js/'))
+        .pipe(dest(paths.dest + 'js/'))
         .pipe(uglify())
         .pipe(rename({ extname: '.min.js' }))
-        .pipe(dest('site/templates/public/js/'));
-}
+        .pipe(dest(paths.dest + 'js/'));
+})
+
+task('build-css', function(cb) {
+    return src(paths.src + 'css/*.css')
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(rename( { extname: '.min.css'}))
+        .pipe(dest(paths.dest + 'css/'));
+})
+
+exports.default = parallel('build-js', 'build-css')
