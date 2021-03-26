cc42f2797fd93744ed153c705f404187bed776bf
Martin Dhenu <martin.dhenu@chefkoch.de>
2021-03-26 18:23:18 +0100

    FRSITE: import js dependencies

--- site/templates/dist/js/main.js
+++ site/templates/dist/js/main.js
@@ -1,3 +1,8 @@
+import $ from 'jquery';
+import 'jquery-migrate/dist/jquery-migrate.min';
+import '@popperjs/core/dist/cjs/popper-base';
+import 'bootstrap/dist/js/bootstrap.min';
 import sayHello from "./components/sayHello";
 
 sayHello("Hello");
+$('.test').text('Render from Jquery')
