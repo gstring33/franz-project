799ccf10dc96bba04a149613ef20ef333224ff24
Martin Dhenu <martin.dhenu@chefkoch.de>
2021-06-25 15:53:05 +0200

    CONTACT: implement xhr structure to send data

---
+++ site/templates/Controller/XhrController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+use App\Core\AbstractController;
+
+class XhrController extends AbstractController
+{
+    public function contact()
+    {
+
+    }
+}
---
+++ site/templates/ajax.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ProcessWire;
+
+use App\Controller\XhrController;
+
+if ($config->ajax()) {
+    $xhrController = new XhrController();
+    switch ($input->urlSegment(1)) {
+        case 'contact':
+            $response = $xhrController->contact();
+            echo $response;
+    }
+}
