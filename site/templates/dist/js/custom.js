10501406ddd11517a629c27197d66d3d4a8a8ef5
Martin Dhenu <martin.dhenu@chefkoch.de>
2021-07-02 11:52:10 +0200

    CONTACT-SECURITY: implement hp control in client

--- site/templates/dist/js/custom.js
+++ site/templates/dist/js/custom.js
@@ -3,41 +3,49 @@
     contactForm.on('submit', (e) => {
         e.preventDefault()
         sanitizeFields()
+
         const formData = {
             name: $('#fr-contact-name').val(),
             email: $('#fr-contact-email').val(),
             message: $('#fr-contact-message').val(),
+            pdm_name: $('#fr-contact-name-pdm').val(),
+            pdm_email: $('#fr-contact-email-pdm').val(),
         }
-        $.ajax({
-            method: 'POST',
-            url: '/xhr/contact',
-            data: formData,
-            dataType: 'json',
-        })
-            .done((response) => {
-                if (response.status === 'success') {
-                    document.querySelector('#fr-contact-form').reset()
-                    const successAlert = '<div class="alert alert-success alert-dismissible fade show g-mb-40" role="alert">' +
-                        '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
-                        '<span aria-hidden="true">×</span>' +
-                        ' </button>' +
-                        '<h4 class="h5">' +
-                        '<i class="fa fa-check-circle-o"></i> ' +
-                        ' Ihre Nachricht wurde erfolgreich gesendet' +
-                        '</h4>' +
-                        response.message +
-                        '</div>'
-                    $(successAlert).insertBefore(contactForm)
-                } else if (response.status === 'error') {
-                    response.errors.forEach((error) => {
-                        const field = error.field
-                        const input = $('#fr-contact-' + field )
-                        input.addClass('form-control-danger').removeClass('g-brd-gray-light-v4')
-                        $('#fr-contact-feedback-' + field).text(error.message)
-                        input.parent('.form-group').addClass('has-danger')
-                    })
-                }
+
+        if (isSpam(formData)) {
+            return
+        } else {
+            $.ajax({
+                method: 'POST',
+                url: '/xhr/contact',
+                data: formData,
+                dataType: 'json',
             })
+                .done((response) => {
+                    if (response.status === 'success') {
+                        document.querySelector('#fr-contact-form').reset()
+                        const successAlert = '<div class="alert alert-success alert-dismissible fade show g-mb-40" role="alert">' +
+                            '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
+                            '<span aria-hidden="true">×</span>' +
+                            ' </button>' +
+                            '<h4 class="h5">' +
+                            '<i class="fa fa-check-circle-o"></i> ' +
+                            ' Ihre Nachricht wurde erfolgreich gesendet' +
+                            '</h4>' +
+                            response.message +
+                            '</div>'
+                        $(successAlert).insertBefore(contactForm)
+                    } else if (response.status === 'error') {
+                        response.errors.forEach((error) => {
+                            const field = error.field
+                            const input = $('#fr-contact-' + field )
+                            input.addClass('form-control-danger').removeClass('g-brd-gray-light-v4')
+                            $('#fr-contact-feedback-' + field).text(error.message)
+                            input.parent('.form-group').addClass('has-danger')
+                        })
+                    }
+                })
+        }
     })
 
     const sanitizeFields = () => {
@@ -47,5 +55,9 @@
             $(this).parent('.form-group').removeClass('has-danger')
             $(this).next('.form-control-feedback').empty()
         })
+    }
+
+    const isSpam = (fields) => {
+        return (fields.pdm_name !== "" || fields.pdm_email !== "")
     }
 })(jQuery)
